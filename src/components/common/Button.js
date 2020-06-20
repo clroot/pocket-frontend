c7c190898d6850470636aa556a472c4b664b5d22
clroot <abcdkh1209@gmail.com>
2020-06-20 19:44:24 +0900

    버튼 스타일 확장

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import palette from '../../lib/styles/palette';
 
 const StyledButton = styled.button`
@@ -16,6 +16,24 @@ const StyledButton = styled.button`
   &:hover {
     background: ${palette.gray[6]};
   }
+
+  ${(props) =>
+    props.fullWidth &&
+    css`
+      padding-top: 0.75rem;
+      padding-bottom: 0.75rem;
+      width: 100%;
+      font-size: 1.125rem;
+    `}
+
+  ${(props) =>
+    props.indigo &&
+    css`
+      background: ${palette.indigo[5]};
+      &:hover {
+        background: ${palette.indigo[4]};
+      }
+    `}
 `;
 
 const Button = (props) => <StyledButton {...props} />;
