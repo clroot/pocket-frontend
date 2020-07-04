ac4a074c061312f6542b378d22bc2d4df1d67f37
clroot <abcdkh1209@gmail.com>
2020-07-04 16:32:06 +0900

    헤더 컴포넌트 구현 & 로그인 유지

--- src/containers/auth/RegisterForm.js
+++ src/containers/auth/RegisterForm.js
@@ -68,6 +68,11 @@ const RegisterForm = ({ history }) => {
   useEffect(() => {
     if (user) {
       history.push('/');
+      try {
+        localStorage.setItem('user', JSON.stringify(user));
+      } catch (error) {
+        console.log('localStorage is not working');
+      }
     }
   }, [user, history]);
 
--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import rootReducer, { rootSaga } from './modules';
+import { tempSetUser, check } from './modules/user';
 
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
@@ -17,7 +18,20 @@ const store = createStore(
   composeWithDevTools(applyMiddleware(sagaMiddleware)),
 );
 
+function loadUser() {
+  try {
+    const user = localStorage.getItem('user');
+    if (!user) return;
+
+    store.dispatch(tempSetUser(user));
+    store.dispatch(check());
+  } catch (error) {
+    console.log('localStorage is not working');
+  }
+}
+
 sagaMiddleware.run(rootSaga);
+loadUser();
 
 ReactDOM.render(
   <Provider store={store}>
