eec8d05151fde4eda35b597a3bb330ab112976e3
clroot <abcdkh1209@gmail.com>
2020-07-04 12:46:56 +0900

    api 연동 준비

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux';
 import auth from './auth';
+import loading from './loading';
 
-const rootReducer = combineReducers({ auth });
+const rootReducer = combineReducers({
+  auth,
+  loading,
+});
 
 export default rootReducer;
