8f8f2db402755cf1279cee567e6b781325d123db
clroot <abcdkh1209@gmail.com>
2020-07-04 16:23:31 +0900

    헤더 컴포넌트 구현

--- src/pages/ArticleListPage.js
+++ src/pages/ArticleListPage.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import Button from '../components/common/Button';
+import Header from '../components/common/Header';
 
 const ArticleListPage = () => {
   return (
     <div>
-      <Button>버튼</Button>
+      <Header />
+      <div>Hi.</div>
     </div>
   );
 };
