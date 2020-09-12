da378d4a2d7b6997532e9c8b9bc8d811137d7313
clroot <abcdkh1209@gmail.com>
2020-09-12 11:13:22 +0900

    tag Link 구현

--- src/components/article/SideMenu.js
+++ src/components/article/SideMenu.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 import Sticky from '../common/Sticky';
 
 const SideMenuBlock = styled.div`
@@ -10,7 +11,12 @@ const SideMenu = ({ tags }) => {
   return (
     <Sticky top={112}>
       <SideMenuBlock>
-        {Array.isArray(tags) && tags.map((tag) => <p>#{tag}</p>)}
+        {Array.isArray(tags) &&
+          tags.map((tag) => (
+            <Link to={`/?tag=${tag}`}>
+              <p key={tag}>#{tag}</p>
+            </Link>
+          ))}
       </SideMenuBlock>
     </Sticky>
   );
