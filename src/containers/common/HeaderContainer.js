36d658aad5095782b6fd85c85884b41339e1205e
clroot <abcdkh1209@gmail.com>
2020-09-13 16:33:42 +0900

    로그아웃시 상태 초기화 및 로그인 페이지로 이동

--- src/containers/common/HeaderContainer.js
+++ src/containers/common/HeaderContainer.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import Header from '../../components/common/Header';
-import { logout } from '../../modules/user';
+import { logout, reset as userReset } from '../../modules/user';
+import { reset as authReset } from '../../modules/auth';
+import { reset as articleReset } from '../../modules/article';
 
-const HeaderContainer = () => {
+const HeaderContainer = ({ history }) => {
   const { user } = useSelector(({ user }) => ({
     user: user.user,
   }));
   const dispatch = useDispatch();
   const onLogout = () => {
     dispatch(logout());
+    dispatch(userReset());
+    dispatch(authReset());
+    dispatch(articleReset());
+    history.push('/login');
   };
 
   return <Header user={user} onLogout={onLogout} />;
 };
 
-export default HeaderContainer;
+export default withRouter(HeaderContainer);
