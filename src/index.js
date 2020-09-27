1158b4a9b16ad3f4de6acccb78a26c451b1387d4
clroot <abcdkh1209@gmail.com>
2020-09-27 18:29:24 +0900

    API서버에서 OAuth 인증 성공 후, front 로그인처리 구현

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
-
+import qs from 'qs';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -20,11 +20,21 @@ const store = createStore(
 
 function loadUser() {
   try {
-    const user = localStorage.getItem('user');
-    if (!user) return;
+    let user;
+    const { loginToken } = qs.parse(window.location.search.slice(1));
+    if (!loginToken) {
+      user = localStorage.getItem('user');
+      if (!user) return;
+    } else {
+      const buffer = Buffer.from(loginToken, 'base64');
+      user = JSON.parse(buffer.toString());
+
+      localStorage.setItem('user', JSON.stringify(user));
+    }
 
     store.dispatch(tempSetUser(user));
     store.dispatch(check());
+    window.history.replaceState({}, document.title, '/');
   } catch (error) {
     console.log('localStorage is not working');
   }
