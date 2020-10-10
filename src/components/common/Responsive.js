e386c58ec12655994deff9c51c5cb1b1ca85c12b
clroot <abcdkh1209@gmail.com>
2020-10-10 16:29:18 +0900

    반응형 breakpoints 수정

--- src/components/common/Responsive.js
+++ src/components/common/Responsive.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
+import * as bp from '../../lib/styles/breakPoints';
 
 const ResponsiveBlock = styled.div`
   overflow-x: hidden;
   padding-left: 1rem;
   padding-right: 1rem;
-  width: 1728px;
   margin: 0 auto;
 
-  @media (max-width: 1919px) {
-    width: 1376px;
-  }
-  @media (max-width: 1440px) {
-    width: 1280px;
-  }
-  @media (max-width: 1312px) {
-    width: 912px;
-  }
-  @media (max-width: 944px) {
+  @media ${bp.medium} {
     width: calc(100% - 2rem);
+  }
+  @media ${bp.large} {
+    max-width: 1440px;
   }
 `;
 
